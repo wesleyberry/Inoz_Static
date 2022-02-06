c10a0a9458be21beaf83b780f22ebb61d81df5ef
Wesley Berry <wesleyberry52@gmail.com>
2022-02-06 14:18:26 -0600

    revised to be static

--- public/assets/scripts/update_dom/bands.js
+++ public/assets/scripts/update_dom/bands.js
@@ -1,96 +1,96 @@
-axios.get('api/bands').then(response => {
-    // Get all bands
-    const results = response.data;
-    for(var i = 0; i < results.length; i++) {
-        var month = results[i].month;
-        switch(month) {
-            case 0:
-                month = 'January';
-                break;
-            case 1:
-                month = 'February';
-                break;
-            case 2:
-                month = 'March';
-                break;
-            case 3:
-                month = 'April';
-                break;
-            case 4:
-                month = 'May';
-                break;
-            case 5:
-                month = 'June';
-                break;
-            case 6:
-                month = 'July';
-                break;
-            case 7:
-                month = 'August';
-                break;
-            case 8:
-                month = 'September';
-                break;
-            case 9:
-                month = 'October';
-                break;
-            case 10:
-                month = 'November';
-                break;
-            case 11:
-                month = 'December';
-                break;
-        }
-        var time = results[i].start+results[i].end;
-        switch(time) {
-            case '1216':
-                time = '12:00 PM - 4:00 PM';
-                break;
-            case '1822':
-                time = '6:00 PM - 10:00 PM';
-                break;
-            case '1922':
-                time = '7:00 PM - 10:00 PM';
-                break;
-        }
-        $('#bandsGoHere').append(
-            `<tr>
-                <th scope="row">${results[i].band}</th>
-                <td>${month} ${results[i].day}, ${results[i].year}</td>
-                <td>${time}</td>
-                <td>
-                    <button type="button" class="btn btn-danger bandDelete" id=${results[i].id}>X</button>
-                </td>
-            </tr>`
-        )
-    }
-}).catch(err => console.log(err));
+// axios.get('api/bands').then(response => {
+//     // Get all bands
+//     // const results = response.data;
+//     for(var i = 0; i < results.length; i++) {
+//         var month = results[i].month;
+//         switch(month) {
+//             case 0:
+//                 month = 'January';
+//                 break;
+//             case 1:
+//                 month = 'February';
+//                 break;
+//             case 2:
+//                 month = 'March';
+//                 break;
+//             case 3:
+//                 month = 'April';
+//                 break;
+//             case 4:
+//                 month = 'May';
+//                 break;
+//             case 5:
+//                 month = 'June';
+//                 break;
+//             case 6:
+//                 month = 'July';
+//                 break;
+//             case 7:
+//                 month = 'August';
+//                 break;
+//             case 8:
+//                 month = 'September';
+//                 break;
+//             case 9:
+//                 month = 'October';
+//                 break;
+//             case 10:
+//                 month = 'November';
+//                 break;
+//             case 11:
+//                 month = 'December';
+//                 break;
+//         }
+//         var time = results[i].start+results[i].end;
+//         switch(time) {
+//             case '1216':
+//                 time = '12:00 PM - 4:00 PM';
+//                 break;
+//             case '1822':
+//                 time = '6:00 PM - 10:00 PM';
+//                 break;
+//             case '1922':
+//                 time = '7:00 PM - 10:00 PM';
+//                 break;
+//         }
+//         $('#bandsGoHere').append(
+//             `<tr>
+//                 <th scope="row">${results[i].band}</th>
+//                 <td>${month} ${results[i].day}, ${results[i].year}</td>
+//                 <td>${time}</td>
+//                 <td>
+//                     <button type="button" class="btn btn-danger bandDelete" id=${results[i].id}>X</button>
+//                 </td>
+//             </tr>`
+//         )
+//     }
+// }).catch(err => console.log(err));
 
 // Deletes band
-$(document).on('click', '.bandDelete', function() {
-    const id = this.id;
-    axios.delete('/api/bands' + id, (req, res) => {})
-    .then(location.reload())
-    .catch(err => console.log(err));
-});
+// $(document).on('click', '.bandDelete', function() {
+//     const id = this.id;
+//     axios.delete('/api/bands' + id, (req, res) => {})
+//     .then(location.reload())
+//     .catch(err => console.log(err));
+// });
 
 // Creates band
-$('#submitBand').on('click', function() {
-    event.preventDefault();
-    var band = $('#bandName').val();
-    var year = $('#year').val();
-    var day = $('#day').val();
-    var month = $('#month').val();
-    var time = $('#time').val().split(' ');
-    var start = time[0];
-    var end = time[1];
-    axios.post('/api/bands', {
-        band,
-        year,
-        day,
-        month,
-        start,
-        end
-    }).then(() => location.reload())
-    .catch(err => console.log(err));
-});
+// $('#submitBand').on('click', function() {
+//     event.preventDefault();
+//     var band = $('#bandName').val();
+//     var year = $('#year').val();
+//     var day = $('#day').val();
+//     var month = $('#month').val();
+//     var time = $('#time').val().split(' ');
+//     var start = time[0];
+//     var end = time[1];
+//     axios.post('/api/bands', {
+//         band,
+//         year,
+//         day,
+//         month,
+//         start,
+//         end
+//     }).then(() => location.reload())
+//     .catch(err => console.log(err));
+// });
--- public/assets/scripts/update_dom/specials.js
+++ public/assets/scripts/update_dom/specials.js
@@ -1,6 +1,15 @@
-axios.get('/api/specialsBar').then(response => {
+// axios.get('/api/specialsBar').then(response => {
     // Updates DOM with bar specials
-    const barSpecials = response.data;
+    // const barSpecials = response.data;
+    const barSpecials = [
+        {day: 'Monday', special: '4.00 Well Drinks'},
+        {day: 'Tuesday', special: '5.00 U-Call It (Excludes frozen, blended, and specialty drinks)'},
+        {day: 'Wednesday', special: '5.00 Regular Frozen or Rocks Margaritas'},
+        {day: 'Thursday', special: '3.00 Domestic / 5.00 Craft Drafts'},
+        {day: 'Friday', special: '10.00 Mextinis'},
+        {day: 'Saturday', special: '11.00 Domestic / 18.00 Craft Draft Pitchers'},
+        {day: 'Sunday', special: '6.00 Bloody Marys'}
+    ];
     $('#barSpecialsGoHere').html(
         `<div class="bd-example background-carousel-1">
         <div id="carouselExampleCaptions-2" class="carousel slide" data-ride="carousel" data-interval="false">
@@ -97,120 +106,120 @@ axios.get('/api/specialsBar').then(response => {
         </div>
     </div>`
     )
-}).catch(err => console.log(err));
+// }).catch(err => console.log(err));
 
 // Updates DOM with food specials
-axios.get('/api/specialsFood').then(response => {
-    const foodSpecials = response.data;
-    if(foodSpecials.length > 0) {
-        $('#foodSpecialsGoHere').html(
-            `<div class="bd-example background-carousel-2">
-            <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel" data-interval="false">
-            <ol class="carousel-indicators">
-                <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
-                <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
-                <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
-                <li data-target="#carouselExampleCaptions" data-slide-to="3"></li>
-                <li data-target="#carouselExampleCaptions" data-slide-to="4"></li>
-                <li data-target="#carouselExampleCaptions" data-slide-to="5"></li>
-                <li data-target="#carouselExampleCaptions" data-slide-to="6"></li>
-            </ol>
-                <div class="carousel-inner">
-                    <h1 class="specialsTitle"><u>Daily Food Specials</u></h1>
-                    <div class="carousel-item active">
-                        <h1 class="specialsDay">${foodSpecials[0].day}</h1>
-                        <h2 class="specials">${foodSpecials[0].special}</h2>
-                    </div>
-                    <div class="carousel-item">
-                        <h1 class="specialsDay">${foodSpecials[1].day}</h1>
-                        <h2 class="specials">${foodSpecials[1].special}</span></h2>
-                    </div>
-                    <div class="carousel-item">
-                        <div class="row justify-content-center">
-                            <div class="col-xs-12 col-lg-4">
-                                <h1 class="specialsDay">${foodSpecials[2].day}</h1>
-                                <h2 class="specials">${foodSpecials[2].special}</h2>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="carousel-item">
-                        <div class="row justify-content-center">
-                            <div class="col-xs-12 col-lg-4">
-                                <h1 class="specialsDay">${foodSpecials[3].day}</h1>
-                                <h2 class="specials">${foodSpecials[3].special}</h2>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="carousel-item">
-                        <div class="row justify-content-center">
-                            <div class="col-xs-12 col-lg-4">
-                                <h1 class="specialsDay">${foodSpecials[4].day}</h1>
-                                <h2 class="specials">${foodSpecials[4].special}</h2>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="carousel-item">
-                        <h1 class="specialsDay">${foodSpecials[5].day}</h1>
-                        <h2 class="specials">${foodSpecials[5].special}</h2>
-                    </div>
-                    <div class="carousel-item">
-                        <div class="row justify-content-center">
-                            <div class="col-xs-12 col-lg-4">
-                                <h1 class="specialsDay">${foodSpecials[6].day}</h1>
-                                <h2 class="specials">${foodSpecials[6].special}</h2>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
-            </a>
-            <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="sr-only">Next</span>
-            </a>
-            </div>
-        </div>`
-        )
-    }
-}).catch(err => console.log(err));
+// axios.get('/api/specialsFood').then(response => {
+//     const foodSpecials = response.data;
+//     if(foodSpecials.length > 0) {
+//         $('#foodSpecialsGoHere').html(
+//             `<div class="bd-example background-carousel-2">
+//             <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel" data-interval="false">
+//             <ol class="carousel-indicators">
+//                 <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+//                 <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+//                 <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+//                 <li data-target="#carouselExampleCaptions" data-slide-to="3"></li>
+//                 <li data-target="#carouselExampleCaptions" data-slide-to="4"></li>
+//                 <li data-target="#carouselExampleCaptions" data-slide-to="5"></li>
+//                 <li data-target="#carouselExampleCaptions" data-slide-to="6"></li>
+//             </ol>
+//                 <div class="carousel-inner">
+//                     <h1 class="specialsTitle"><u>Daily Food Specials</u></h1>
+//                     <div class="carousel-item active">
+//                         <h1 class="specialsDay">${foodSpecials[0].day}</h1>
+//                         <h2 class="specials">${foodSpecials[0].special}</h2>
+//                     </div>
+//                     <div class="carousel-item">
+//                         <h1 class="specialsDay">${foodSpecials[1].day}</h1>
+//                         <h2 class="specials">${foodSpecials[1].special}</span></h2>
+//                     </div>
+//                     <div class="carousel-item">
+//                         <div class="row justify-content-center">
+//                             <div class="col-xs-12 col-lg-4">
+//                                 <h1 class="specialsDay">${foodSpecials[2].day}</h1>
+//                                 <h2 class="specials">${foodSpecials[2].special}</h2>
+//                             </div>
+//                         </div>
+//                     </div>
+//                     <div class="carousel-item">
+//                         <div class="row justify-content-center">
+//                             <div class="col-xs-12 col-lg-4">
+//                                 <h1 class="specialsDay">${foodSpecials[3].day}</h1>
+//                                 <h2 class="specials">${foodSpecials[3].special}</h2>
+//                             </div>
+//                         </div>
+//                     </div>
+//                     <div class="carousel-item">
+//                         <div class="row justify-content-center">
+//                             <div class="col-xs-12 col-lg-4">
+//                                 <h1 class="specialsDay">${foodSpecials[4].day}</h1>
+//                                 <h2 class="specials">${foodSpecials[4].special}</h2>
+//                             </div>
+//                         </div>
+//                     </div>
+//                     <div class="carousel-item">
+//                         <h1 class="specialsDay">${foodSpecials[5].day}</h1>
+//                         <h2 class="specials">${foodSpecials[5].special}</h2>
+//                     </div>
+//                     <div class="carousel-item">
+//                         <div class="row justify-content-center">
+//                             <div class="col-xs-12 col-lg-4">
+//                                 <h1 class="specialsDay">${foodSpecials[6].day}</h1>
+//                                 <h2 class="specials">${foodSpecials[6].special}</h2>
+//                             </div>
+//                         </div>
+//                     </div>
+//                 </div>
+//             <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+//                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+//                 <span class="sr-only">Previous</span>
+//             </a>
+//             <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+//                 <span class="carousel-control-next-icon" aria-hidden="true"></span>
+//                 <span class="sr-only">Next</span>
+//             </a>
+//             </div>
+//         </div>`
+//         )
+//     }
+// }).catch(err => console.log(err));
 
 // Get all specials
-axios.get('api/specials').then(response => {
-    const results = response.data;
-    for(var i = 0; i < results.length; i++) {
-        $('#specialsGoHere').append(
-            `<tr>
-                <th scope="row">${results[i].day}</th>
-                <td>${results[i].special}</td>
-                <td>${results[i].type}</td>
-                <td>
-                    <button type="button" class="btn btn-danger specialsDelete" id=${results[i].id}>X</button>
-                </td>
-            </tr>`
-        )
-    }
-}).catch(err => console.log(err));
+// axios.get('api/specials').then(response => {
+//     const results = response.data;
+//     for(var i = 0; i < results.length; i++) {
+//         $('#specialsGoHere').append(
+//             `<tr>
+//                 <th scope="row">${results[i].day}</th>
+//                 <td>${results[i].special}</td>
+//                 <td>${results[i].type}</td>
+//                 <td>
+//                     <button type="button" class="btn btn-danger specialsDelete" id=${results[i].id}>X</button>
+//                 </td>
+//             </tr>`
+//         )
+//     }
+// }).catch(err => console.log(err));
 
 // Deletes special
-$(document).on('click', '.specialsDelete', function() {
-    const id = this.id;
-    axios.delete('/api/specials' + id, (req, res) => {})
-    .then(location.reload())
-    .catch(err => console.log(err));
-});
+// $(document).on('click', '.specialsDelete', function() {
+//     const id = this.id;
+//     axios.delete('/api/specials' + id, (req, res) => {})
+//     .then(location.reload())
+//     .catch(err => console.log(err));
+// });
 
 // New special
-$('#submitSpecial').on('click', function() {
-    event.preventDefault();
-    var day = $('#exampleFormControlSelect1').val();
-    var special = $('#specialInput').val();
-    var type = $('#exampleFormControlSelect2').val();
-    axios.post('/api/specials', {
-        day, 
-        special,
-        type
-    }).then(() => location.reload())
-    .catch(err => console.log(err));
-});
+// $('#submitSpecial').on('click', function() {
+//     event.preventDefault();
+//     var day = $('#exampleFormControlSelect1').val();
+//     var special = $('#specialInput').val();
+//     var type = $('#exampleFormControlSelect2').val();
+//     axios.post('/api/specials', {
+//         day, 
+//         special,
+//         type
+//     }).then(() => location.reload())
+//     .catch(err => console.log(err));
+// });
